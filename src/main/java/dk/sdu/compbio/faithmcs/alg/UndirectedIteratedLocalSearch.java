7326a516785dac8de1a3d29b5c9071a1a5e19bfd
Patrick Wang <wang.patrick57@gmail.com>
2023-01-27 10:11:40 -0800

    logging local search

--- src/main/java/dk/sdu/compbio/faithmcs/alg/UndirectedIteratedLocalSearch.java
+++ src/main/java/dk/sdu/compbio/faithmcs/alg/UndirectedIteratedLocalSearch.java
@@ -90,8 +90,10 @@ public class UndirectedIteratedLocalSearch implements IteratedLocalSearch {
 
         // local search step
         boolean repeat = true;
+        int numSwaps = 0;
         while(repeat) {
             repeat = false;
+            System.out.println("performed " + (numSwaps / n) + " swaps per network");
             for (int i = 1; i < n; ++i) {
                 for (int j = 0; j < M-1; ++j) {
                     int finalI = i;
@@ -112,6 +114,7 @@ public class UndirectedIteratedLocalSearch implements IteratedLocalSearch {
                     if(dt > 0) {
                         repeat = true;
                         swap(indices.get(i), nodes.get(i).get(j), nodes.get(i).get(best));
+                        ++numSwaps;
                     }
                 }
             }
